287a666529ba811c6153c1803ffc7b343ca9af8e
ben strawbridge <ben@benstrawbridge.com>
2024-01-05 17:26:50 -0800

    plain vanilla reboot with just my own theme i'm starting
    content is just coming together now.

---
+++ content/galleries/index.md
@@ -0,0 +1,6 @@
++++
+title = 'Galleries'
+summary = 'Host a new webiste for every artist or exhibition'
+date = 2024-01-05T17:19:12-08:00
+draft = false
++++
---
+++ themes/benstraw/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["content/**/*.md", "layouts/**/*.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'maui-sunset': " url('/images/maui-sunset.webp')",
+      },},
+  },
+  plugins: [require("@tailwindcss/typography")],
+}
+
